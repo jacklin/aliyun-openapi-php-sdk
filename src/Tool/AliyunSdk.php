03a932913a18a35a133f2777936835cd8c817c1f
超级手艺人 <fjxmljc007@163.com>
2020-01-20 17:06:38 +0800

    去除无用的属性

--- src/Tool/AliyunSdk.php
+++ src/Tool/AliyunSdk.php
@@ -3,47 +3,42 @@ namespace Tool;
 define('ALIYUN_OPENAPI_PHP_SDK',__DIR__.'/aliyun-openapi-php-sdk/');
 include_once ALIYUN_OPENAPI_PHP_SDK.'aliyun-php-sdk-core/Config.php';
 
-use Alidns\Request\V20150109;
-use BatchCompute\Request\V20130111;
-use Cdn\Request\V20180510;
-use CS\Request\V20151215;
-use Dm\Request\V20151123;
-use Domain\Request\V20160511;
-use Ecs\Request\V20140526;
-use Green\Request\V20161216;
-use Iot\Request\V20160530;
-use jap\Request\V20161123;
-use Kms\Request\V20160120;
-use live\Request\V20161101;
-use Mts\Request\V20140618;
-use Push\Request\V20160801;
-use Rds\Request\V20140815;
-use Slb\Request\V20140515;
-use Sms\Request\V20160827;
-use Sts\Request\V20150401;
-use Ubsms\Request\V20150623;
-
-
 /**
-* 
-*/
+ * 阿里云SDK通类的实例类
+ */
 class AliyunSdk
 {
 	private $accessKeyId;
 	private $accessKeySecret;
 	private $appKey;
-	private $client; //对就SDK客户端对象
-	public function __construct($accessKeyId, $accessKeySecret, $appKey='',$regionId='cn-hangzhou'){
+
+	/**
+	 * 构造方法
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2020-01-20T16:55:04+0800
+	 * @param    string                   $accessKeyId     访问ID
+	 * @param    string                   $accessKeySecret 访问密钥
+	 * @param    string                   $appKey          应用私钥
+	 */
+	public function __construct($accessKeyId, $accessKeySecret, $appKey=''){
 			// 设置你自己的AccessKeyId/AccessSecret/AppKey
 			$this->accessKeyId = $accessKeyId;
 			$this->accessKeySecret = $accessKeySecret;
 			$this->appKey = $appKey;
 	}
 	
+	/**
+	 * 魔术回调方法
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2020-01-20T16:56:15+0800
+	 * @param    string                   $name      实例SDK请求项
+	 * @param    array                    $arguments 参数：V20180509 ImageSyncScanRequest
+	 *                                               版本 实例相应请求类
+	 * @return   mixed                              实例相应请求类
+	 */
 	public function __call(string $name, $arguments=[]){
-		$iClientProfile = \DefaultProfile::getProfile("cn-hangzhou", $this->accessKeyId, $this->accessKeySecret);
-		$this->setClient(new \DefaultAcsClient($iClientProfile));
-
 		$mid_suffix = array_shift($arguments);
 		$suffix = array_shift($arguments);
 		$class_name = ucfirst($name).'\\Request\\'.$mid_suffix.'\\'.$suffix;
@@ -57,12 +52,5 @@ class AliyunSdk
 		}else{
 			throw new \Exception("调用的方法无法实例化类:".$class_name);
 		}
-	}
-	public function getClient(){
-		return $this->client;
-	}
-	protected function setClient(\DefaultAcsClient $client){
-		$this->client = $client;
-		return $this;
 	}
 }
